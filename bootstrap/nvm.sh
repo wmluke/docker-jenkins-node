1ddf23bcdb6f757bc3e1ca5ecb530b6553848c19
wmluke <wmlukeb@gmail.com>
2016-11-08 13:57:05 -0800

    Updated the default node version to 6.9.1

--- bootstrap/nvm.sh
+++ bootstrap/nvm.sh
@@ -2,7 +2,8 @@
 
 . /opt/nvm/nvm.sh;
 nvm install v4.4.3;
-nvm alias default v4.4.3;
+nvm install v6.9.1;
+nvm alias default v6.9.1;
 npm install -g grunt-cli;
 npm install -g bower;
 npm install -g phantomjs-prebuilt;
